29741c97a9e312c45381158013653a7aee226350
Luke Policinski <luke@lukepolo.com>
2017-03-28 15:49:48 -0400

    more taiga setup

--- shell/commit.sh
+++ shell/commit.sh
@@ -3,10 +3,15 @@
 echo What issue are you working on?
 read issue
 echo What did you work on?
-read commit
+read message
+
+if [ ! -z "$issue" ]
+then
+    issue="TG-$issue : "
+fi
 
 br=`git branch | grep "*"`
 
 git add --all
-git commit -m "TG-$issue $commit"
+git commit -m "$issue$message"
 git push origin ${br/* /}
