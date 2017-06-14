884d07e86bbdefc8495a9bb3ff1810c8b2e0ad57
Luke Policinski <luke@lukepolo.com>
2017-06-14 15:45:12 -0400

    just minor clean ups

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -7,10 +7,6 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-###############################################################################
-# General UI/UX                                                               #
-###############################################################################
-
 # hibernation
 # 0 - suspend to RAM only (default on desktops)
 # 1 - suspend to disk only
